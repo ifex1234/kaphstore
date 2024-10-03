9c09d3b3510e5268fd96b47d7473f598af38cd33
Ifechukwu Peter <ifecko2dumbi@gmail.com>
2024-10-03 23:26:06 +0100

    moved from local host

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -24,7 +24,7 @@ type FectchUser = z.infer<typeof getUserSchema>;
 export const fetchUser = async (userId: string): Promise<FectchUser> => {
   const option = {
     method: "GET",
-    url: `http://127.0.0.1:3001/api/users/${userId}`,
+    url: `https://kaphstore-server.onrender.com/api/users/${userId}`,
   };
   console.log("Fetching user...");
   const requestData = await axios.request(option).then((r) => r.data);
@@ -35,7 +35,7 @@ export const fetchUser = async (userId: string): Promise<FectchUser> => {
 export const fetchProducts = async (category: string) => {
   const option = {
     method: "GET",
-    url: `http://127.0.0.1:3001/api/products/all-products/${category}`,
+    url: `https://kaphstore-server.onrender.com/api/products/all-products/${category}`,
   };
   console.log("Fetching items...");
   const requestData = await axios.request(option).then((r) => r.data);
@@ -46,7 +46,7 @@ export const fetchProducts = async (category: string) => {
 export const fetchProduct = async (id: string) => {
   const option = {
     method: "GET",
-    url: `http://127.0.0.1:3001/api/products/${id}`,
+    url: `https://kaphstore-server.onrender.com/api/products/${id}`,
   };
   const data = axios.request(option).then((r) => r.data);
   const header = axios.request(option).then((r) => r.headers);
@@ -66,7 +66,7 @@ type OrderProp = {
 export const fetchOrderByUser = async (userId: string): Promise<OrderProp> => {
   const option = {
     method: "GET",
-    url: `http://127.0.0.1:3001/api/orders/all-orders/${userId}`,
+    url: `https://kaphstore-server.onrender.com/api/orders/all-orders/${userId}`,
   };
   const data = await axios.request(option).then((r) => r.data);
   return data;
@@ -76,7 +76,7 @@ export const fetchOrderByOrderId = async (
 ): Promise<orderSchema> => {
   const option = {
     method: "GET",
-    url: `http://127.0.0.1:3001/api/orders/all-orders/${orderId}`,
+    url: `https://kaphstore-server.onrender.com/api/orders/all-orders/${orderId}`,
   };
   const data = await axios.request(option).then((r) => r.data);
   console.log(data);
